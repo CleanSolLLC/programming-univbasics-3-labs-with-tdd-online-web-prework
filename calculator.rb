39e6b579d40a66005a64499d34edb04020a59c3c
Mark May <mamark90@gmail.com>
2019-08-02 20:19:11 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -1,5 +1,5 @@
 first_number = 5
 second_number = 9
-differnece = 0
 sum = first_number + second_number
-differnece = first_number - second_number
+difference = first_number - second_number
+product = first_number * second_number
